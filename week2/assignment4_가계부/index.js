0e4928feed0221d2a4ea469163776d1d408a6ec6
se0jinYoon <tjwls1999@gmail.com>
2023-10-20 15:17:55 +0900

    [feat] 총수입, 총지출 js로 변경

--- week2/assignment4_가계부/index.js
+++ week2/assignment4_가계부/index.js
@@ -12,68 +12,77 @@ const HISTORY_LIST = [
     content: '수입',
     place: '당근마켓 거래',
     inOrOut: 'in',
-    amount: '13,000'
+    amount: '13,000',
   },
   {
-    id:3,
+    id: 3,
     content: '간식',
     place: '세븐일레븐 양재시장',
     inOrOut: 'out',
-    amount: '25,000'
+    amount: '25,000',
   },
   {
-    id:4,
+    id: 4,
     content: '용돈',
     place: '가족계좌',
     inOrOut: 'in',
-    amount: '100,000'
-  }
+    amount: '100,000',
+  },
 ];
 
-let totalBalance = INIT_BALANCE;
 document.addEventListener('DOMContentLoaded', () => {
-const ulElement = document.getElementById('inout-list');
-const init_balanceElement = document.getElementById('init_balance');
+  let totalBalance = INIT_BALANCE;
 
-HISTORY_LIST.forEach(item => {
-  const liElement = document.createElement('li');
-  const closeButton = document.createElement('button');
-  closeButton.className = 'close';
-  closeButton.textContent = 'x';
+  let income_amount = 0;
+  let outcome_amount = 0;
+  const income_amount_Element = document.getElementById('income_amount');
+  const outcome_amount_Element = document.getElementById('outcome_amount');
 
-  const detailDiv = document.createElement('div');
-  detailDiv.className = 'detail';
+  const ulElement = document.getElementById('inout-list');
+  const init_balanceElement = document.getElementById('init_balance');
 
-  const contentSpan = document.createElement('span');
-  contentSpan.className = 'content';
-  contentSpan.textContent = item.content;
+  HISTORY_LIST.forEach((item) => {
+    const liElement = document.createElement('li');
+    const closeButton = document.createElement('button');
+    closeButton.className = 'close';
+    closeButton.textContent = 'x';
 
-  const placeSpan = document.createElement('span');
-  placeSpan.className = 'place';
-  placeSpan.textContent = item.place;
+    const detailDiv = document.createElement('div');
+    detailDiv.className = 'detail';
 
-  const amountSpan = document.createElement('span');
-  if (item.inOrOut === 'out') {
-    amountSpan.className = 'out-list';
-    amountSpan.textContent = `-${item.amount}`;
-    
-    totalBalance -= Number(item.amount.replace(/,/g,""));
-  } else if (item.inOrOut === 'in') {
-    amountSpan.className = 'in-list';
-    amountSpan.textContent = `+${item.amount}`;
-    
-    totalBalance += Number(item.amount.replace(/,/g,""));
-  }
+    const contentSpan = document.createElement('span');
+    contentSpan.className = 'content';
+    contentSpan.textContent = item.content;
 
-  detailDiv.appendChild(contentSpan);
-  detailDiv.appendChild(placeSpan);
-  detailDiv.appendChild(amountSpan);
-  liElement.appendChild(closeButton);
-  liElement.appendChild(detailDiv);
+    const placeSpan = document.createElement('span');
+    placeSpan.className = 'place';
+    placeSpan.textContent = item.place;
 
-  ulElement.appendChild(liElement);
+    const amountSpan = document.createElement('span');
+    if (item.inOrOut === 'out') {
+      amountSpan.className = 'out-list';
+      amountSpan.textContent = `-${item.amount}`;
 
+      totalBalance -= Number(item.amount.replace(/,/g, ''));
+      outcome_amount -= Number(item.amount.replace(/,/g, ''));
+    } else if (item.inOrOut === 'in') {
+      amountSpan.className = 'in-list';
+      amountSpan.textContent = `+${item.amount}`;
+
+      totalBalance += Number(item.amount.replace(/,/g, ''));
+      income_amount += Number(item.amount.replace(/,/g, ''));
+    }
+
+    detailDiv.appendChild(contentSpan);
+    detailDiv.appendChild(placeSpan);
+    detailDiv.appendChild(amountSpan);
+    liElement.appendChild(closeButton);
+    liElement.appendChild(detailDiv);
+
+    ulElement.appendChild(liElement);
+  });
   init_balanceElement.innerText = totalBalance;
-});
-})
 
+  income_amount_Element.innerText = income_amount;
+  outcome_amount_Element.innerText = outcome_amount.toString().replace("-", " ");
+});
