4dd918afdd6676ce4798f201cc753ca5895812a3
se0jinYoon <tjwls1999@gmail.com>
2023-11-16 10:34:24 +0900

    style: global style, theme 적용

--- week4/SignupLogin/src/App.jsx
+++ week4/SignupLogin/src/App.jsx
@@ -1,9 +1,18 @@
+import styled, {ThemeProvider} from 'styled-components';
+import GlobalStyle from './style/GlobalStyle';
+import theme from './style/theme';
+
 function App() {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      
+    </ThemeProvider>
     </>
   )
 }
 
 export default App
+
---
+++ week4/SignupLogin/src/style/theme.js
@@ -0,0 +1,17 @@
+const colors = {
+    white: '#FFFFFF',
+    yellow: '#FEE500',
+    gray: '#E4E4DD',
+    black: '#000000',
+};
+
+const font = {
+    fontFamily: 'Pretendard-Regular',
+};
+
+const theme = {
+    colors,
+    font,
+};
+
+export default theme;
