5ccae6cb5f6f0c77e07d2e3e92897726d77f1d5a
se0jinYoon <tjwls1999@gmail.com>
2023-11-16 11:32:24 +0900

    feat: InputWrapper 레이아웃 구현

--- week4/SignupLogin/src/App.jsx
+++ week4/SignupLogin/src/App.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import styled, {ThemeProvider} from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './style/GlobalStyle';
 import theme from './style/theme';
 
 import ContentWrapper from './components/Layout/ContentWrapper';
+import InputWrapper from './components/Layout/InputWrapper';
 import Input from './components/UI/Input';
 
 function App() {
-
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
 
-      <ContentWrapper header={'로그인'}>
-        <Input label={'로그인'} placeholder={'아이디를 입력하세요'}></Input>
-      </ContentWrapper>
-      
-    </ThemeProvider>
+        <ContentWrapper header={'로그인'}>
+          <InputWrapper>
+            <Input label={'로그인'} placeholder={'아이디를 입력하세요'}></Input>
+            <Input label={'로그인'} placeholder={'아이디를 입력하세요'}></Input>
+          </InputWrapper>
+        </ContentWrapper>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
